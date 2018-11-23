c18eb12a006d0c5c577f69a608c0c22de7a111ca
MohamadKh75 [windows] <khajavi_mohamad@yahoo.com>
2018-11-23 16:46:43 +0330

    Add fromRight

--- src/index.js
+++ src/index.js
@@ -28,6 +28,34 @@ export function fromLeft(duration = 300) {
   };
 }
 
+export function fromRight(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true
+    },
+
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initWidth } = layout;
+
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [+initWidth, 0, 0]
+      });
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index - 0.99, index],
+        outputRange: [0, 1, 1]
+      });
+
+      return { opacity, transform: [{ translateX }] };
+    }
+  };
+}
+
 export function fromBottom(duration = 300) {
   return {
     transitionSpec: {
