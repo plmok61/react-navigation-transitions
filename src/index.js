3f1b8d7cfe54b90fcd9fc2acd08cc6aec944a541
Phil Mok <plmok61@gmail.com>
2019-01-22 17:48:46 -0600

    Adds fromRight and fromBottom for Android (#9)
    * Adds fromRight and fromBottom for Android
    
    * 1.0.7

--- src/index.js
+++ src/index.js
@@ -54,6 +54,60 @@ export function fromTop(duration = 300) {
   };
 }
 
+export function fromRight(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true,
+    },
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initWidth } = layout;
+
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [initWidth, 0, 0],
+      });
+
+      const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        });
+
+      return { opacity, transform: [{ translateX }] };
+    },
+  };
+}
+
+export function fromBottom(duration = 300) {
+  return {
+    transitionSpec: {
+      duration,
+      easing: Easing.out(Easing.poly(4)),
+      timing: Animated.timing,
+      useNativeDriver: true,
+    },
+    screenInterpolator: ({ layout, position, scene }) => {
+      const { index } = scene;
+      const { initHeight } = layout;
+
+      const translateY = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [initHeight, 0, 0],
+      });
+
+      const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        });
+
+      return { opacity, transform: [{ translateY }] };
+    },
+  };
+}
+
 export function fadeIn(duration = 300) {
   return {
     transitionSpec: {
